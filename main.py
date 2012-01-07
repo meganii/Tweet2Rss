ed2f986a6cc3aff1be5aa1aeaac52b3cf44410d5
meganii <yuhei24@gmail.com>
2012-01-07 15:56:15 +0900

    import re

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# -*- encoding: utf-8 -*-
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+import re
 import tweepy
 import ConfigParser
 
@@ -28,6 +17,13 @@ consumer_secret = config.get('env','consumer_secret')
 access_key      = config.get('env','access_key')
 access_secret   = config.get('env','access_secret')
 
+
+class Tweet(db.Model):
+    id =  db.IntegerProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+    content = db.StringProperty(multiline=True)
+    url = db.StringProperty(multiline=True)
+
 class MainHandler(webapp.RequestHandler):
     def get(self):
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -36,7 +32,11 @@ class MainHandler(webapp.RequestHandler):
 
         list_tl = api.list_timeline(owner='meganii',slug='lifehacks')
         for tweets in list_tl:
-            self.response.out.write(tweets.text)
+            m = re.search("(http://[A-Za-z0-9\'~+\-=_.,/%\?!;:@#\*&\(\)]+)", tweets.text)
+            if m:
+                tweet = Tweet()
+                tweet.content = tweets.text
+                self.response.out.write(tweets.text)
 
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)],
